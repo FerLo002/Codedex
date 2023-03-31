9bfd1a6ec6a999f0d140e7171ce64b6d011458e4
Fernando López <127046384+FerLo002@users.noreply.github.com>
2023-03-31 15:51:16 -0600

    letter.py
    letter for my future self

---
+++ letter.py
@@ -0,0 +1,12 @@
+"""
+1.-Today's date.
+2.-How you are feeling right now.
+3.-What you want to accomplish during this course.
+4.-A little message to your older, wiser, and programmer self.
+5.-Your favorite emoji to spice things up!
+"""
+print('Friday, March 31, 2023')
+print("I'm feeling excited because today it's my first day of vacation")
+print('I want to be a better programmer in python because i think that is a great programing languaje')
+print('Remember that you started to learn without any knowledge, many people did help you, you must help new people')
+print('𓆏')
